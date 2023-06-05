14f0dad59430388683830ba18a5369fa7b14d91d
seungchan baek <poketsc@gmail.com>
2023-06-05 12:48:44 +0900

    인터페이스 상속, 다형성 스터디

---
+++ Chapter3/src/ch13/domain/userInfo/UserInfo.java
@@ -0,0 +1,29 @@
+package ch13.domain.userInfo;
+
+public class UserInfo {
+	
+	private String userId;
+	private String passWord;
+	private String userName;
+	
+	public String getUserId() {
+		return userId;
+	}
+	public void setUserId(String userId) {
+		this.userId = userId;
+	}
+	public String getPassWord() {
+		return passWord;
+	}
+	public void setPassWord(String passWord) {
+		this.passWord = passWord;
+	}
+	public String getUserName() {
+		return userName;
+	}
+	public void setUserName(String userName) {
+		this.userName = userName;
+	}
+	
+	
+}
